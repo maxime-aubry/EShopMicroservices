d80d87c331ab78f817fc628f04a8a5e3a00accaa
Maxime AUBRY <maxime.aubry.76@gmail.com>
2025-06-12 16:48:27 +0200

    ajout de multiples endpoint CRUD sur les Product

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -5,7 +5,7 @@ namespace Catalog.API.Products.GetProducts
 
     public record GetProductsResult(IEnumerable<Product> Products);
 
-    public class GetProductsQueryHandler
+    internal class GetProductsQueryHandler
         (IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
         : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
