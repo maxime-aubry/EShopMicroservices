97644e2df270965de95a23d9ee8527134b17f9c5
Maxime AUBRY <maxime.aubry@akkodis.com>
2025-06-12 21:38:41 +0200

    complete delete endpoint

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿
+namespace Catalog.API.Products.DeleteProduct
 {
-    public class DeleteProductEndpoint
+    public record DeleteProductResponse(bool IsSuccess);
+
+    public class DeleteProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                DeleteProductResult? result = await sender.Send(new DeleteProductCommand(id));
+                DeleteProductResponse? response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
     }
 }
