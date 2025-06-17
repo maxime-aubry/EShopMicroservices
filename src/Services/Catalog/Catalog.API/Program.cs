6272ebf8c53d6ae1ecebf3dc033f9c851986a4b8
Maxime AUBRY <maxime.aubry.76@gmail.com>
2025-06-17 10:05:26 +0200

    ajout d'en endpoint /health pour vérifier l'état de la base de données

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,12 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using System.Reflection;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 Assembly assembly = typeof(Program).Assembly;
+string dbConnexionString = builder.Configuration.GetConnectionString("Database")!;
 
 builder.Services.AddMediatR(config =>
 {
@@ -17,7 +20,7 @@ builder.Services.AddCarter();
 
 builder.Services.AddMarten(options =>
 {
-    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Connection(dbConnexionString);
 }).UseLightweightSessions();
 
 if (builder.Environment.IsDevelopment())
@@ -25,11 +28,20 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(dbConnexionString);
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
 app.UseExceptionHandler((IApplicationBuilder options) => {});
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
